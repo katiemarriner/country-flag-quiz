984521b89b7a2594a99c39d91b5fa40ca98119ba
Katie Marriner <kemarriner@gmail.coml>
2025-05-13 17:23:26 -0400

    update config and render some stuff to test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,12 @@
 </script>
 
 <h1>What country does this flag belong to?</h1>
+{#each data as flag}
+  <div>
+    <span>{flag.name}</span>
+    <span>{flag.emoji}</span>
+  </div>
+{/each}
 <!-- Play area -->
 <!-- Randomize Flag -->
 <!-- Choices or text input -->
